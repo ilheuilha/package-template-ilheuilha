73513fc210889d9927e7ca0d0a277399dbb4a241
ilheuilha <junior_ilheu@hotmail.com>
2024-08-26 16:57:47 -0300

    Curso pacote

--- README.md
+++ README.md
@@ -1,27 +1,17 @@
 # package_name
 
 Description. 
-The package package_name is used to:
-	- 
-	-
+The package package_financas is used to:
+	- Pandas
+	- matplotlib
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_financas
 
 ```bash
-pip install package_name
-```
-
-## Usage
-
-```python
-from package_name.module1_name import file1_name
-file1_name.my_function()
+pip install financas
 ```
 
 ## Author
-My_name
-
-## License
-[MIT](https://choosealicense.com/licenses/mit/)
+Ilheuilha
--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import setup, find_packages
 
+
 with open("README.md", "r") as f:
     page_description = f.read()
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+
 setup(
-    name="package_name",
+    name="package_python",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="ilheuilha",
+    author_email="teste@teste.com",
+    description="pequeno programa para verificar tipos de investimentos",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/ilheuilha/package-template-ilheuilha.git",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
